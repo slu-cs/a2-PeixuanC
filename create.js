5797fae82c330c81113eb23ad18c8602f15ef965
Peixuan Cai <pcai17@stlawu.edu>
2020-02-21 13:03:47 -0500

    2.21

---
+++ create.js
@@ -0,0 +1,34 @@
+
+
+// Store some data in the faculty database
+
+const mongoose = require('mongoose');
+const connect = require('./db');
+const Voter = require('./schema');
+const fs = require('fs');
+const readline = require('readline');
+const file = readline.createInterface({
+  input: fs.createReadStream('voters.csv')
+});
+
+connect(); // To the database
+
+const voters = [];
+file.on('line', function(line) {
+  const columns = line.split(',');
+  voters.push({const voter = new Voter({
+    first: columns[0],
+    last: columns[1],
+    zip: columns[2],
+    history:colums[3]
+  });
+});
+
+
+const saves = voters.map(d => d.save());
+mongoose.connection.dropDatabase()
+Promise.all(saves)
+  .then(() => console.log('All saved'))
+   .then(() => mongoose.connection.close())
+   .then(() => console.log('Database is ready.'))
+  .catch(error => console.log(error.stack));
---
+++ schema.js
@@ -0,0 +1,19 @@
+// Define a plan for a collection
+
+const mongoose = require('mongoose');
+
+// Schema for a collection of professors
+const Voter = new mongoose.Schema({
+  first: String,
+  last: String,
+  zip: String,
+  history: String
+});
+
+
+
+// Speed up queries on all fields
+Voter.index({first: 1});
+Voter.index({last: 1});
+Voter.index({zip: 1});
+Voter.index({history: 1});
