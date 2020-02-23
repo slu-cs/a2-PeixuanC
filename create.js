5bad30694fa7e31d4bfc6cb16c8fa5ae7c6e4886
Peixuan Cai <pcai17@stlawu.edu>
2020-02-23 13:49:30 -0500

    2.23

--- create.js
+++ create.js
@@ -26,7 +26,7 @@ file.on('line', function(line) {
 
 file.on('close', function() {
   mongoose.connection.dropDatabase()
-    .then(() => voters.map(d => d.save())))
+  .then(()=> voters.map(d=>d.save()))
     .then(() => mongoose.connection.close())
     .then(() => console.log('Database is ready.'))
     .catch(error => console.log(error.stack));
