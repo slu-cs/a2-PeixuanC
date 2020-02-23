43b69163a5575abb08b84bc00d066f706778548e
Peixuan Cai <pcai17@stlawu.edu>
2020-02-23 16:39:47 -0500

    2.23

--- create.js
+++ create.js
@@ -16,11 +16,18 @@ connect(); // To the database
 const voters = [];
 file.on('line', function(line) {
   const columns = line.split(',');
+  const str=columns[3];
+  const election=[];
+  while(str.length>0){
+    election.push(str.substr(0,3));
+    str=str.substr(3);
+  }
+
   voters.push( new Voter({
     first: columns[0],
     last: columns[1],
     zip: columns[2],
-    history:columns[3].slice(3),
+    history:election,
   }));
 });
 
