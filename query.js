0581a7e9a51073a9fe7904a86e2bbd58047e772d
Peixuan Cai <pcai17@stlawu.edu>
2020-02-23 14:23:03 -0500

    2.232

--- query.js
+++ query.js
@@ -16,12 +16,12 @@ query.exec(function(error, professors) {
 const queries = [
 
   //How many registered voters live in the Canton zip code (13617)?
-  Voter.find().where('zip').equals("13617"),
+  Voter.find().where('zip').equals("13617").count(),
 //What are the full names of all the registered voters whose first-name is STARR?
   Voter.find().where('first').equals("STARR"),
 
 //How many people voted in the 2016 general election (GE16)?
-  Voter.find().where('history').includes("world")
+  Voter.find().where('history').includes("GE16").count(),
 //What is the last-name that comes last in the county in alphabetical order?
 
 
@@ -31,9 +31,9 @@ const queries = [
 // Run the queries in parallel
 Promise.all(queries)
   .then(function(results) {
-    console.log('# of voter of 13617: ', results[0].length);
+    console.log('# of voter of 13617: ', results[0]);
     console.log('full names of all the registered voters whose first-name is STARR: ', results[1].map(p => p.first+p.last));
-    console.log('# of people voted in the 2016:', results[2].length);
+    console.log('# of people voted in the 2016:', results[2]);
   //  console.log('Teaches 362: ', results[3].map(p => p.name));
   //  console.log('Distinct ranks: ', results[4]);
     mongoose.connection.close();
