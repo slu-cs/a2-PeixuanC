5e9142921cbcadc37cc38cba7b82d5145e169514
Peixuan Cai <pcai17@stlawu.edu>
2020-02-23 15:54:16 -0500

    2.23

--- query.js
+++ query.js
@@ -25,6 +25,8 @@ const queries = [
 //What is the last-name that comes last in the county in alphabetical order?
 Voter.find().sort('-last').limit(1),
 
+Voter.distinct('zip')
+
 
 
 ];
@@ -36,6 +38,6 @@ Promise.all(queries)
     console.log('full names of all the registered voters whose first-name is STARR: ', results[1].map(p => p.first+p.last));
     console.log('# of people voted in the 2016:', results[2]);
    console.log('the last-name that comes last in the county in alphabetical order', results[3].map(p => p.last));
-  //  console.log('Distinct ranks: ', results[4]);
+    console.log('Distinct zip: ', results[4]);
     mongoose.connection.close();
   }).catch(error => console.error(error.stack));
